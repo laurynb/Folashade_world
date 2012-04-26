b3c345490c3101054a02e8e0f6b24c5aaac99b11
laurynb <laurynb@andrew.cmu.edu>
2012-04-25 20:58:51 -0400

    Created article model

---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ActiveRecord::Base
+  belongs_to :category
+  validates_presence_of :title, :content 
+  scope :active, where('active = ?', true)
+  scope :alphabetical, order('title')
+end
